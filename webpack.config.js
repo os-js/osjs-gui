c4a46e7242f5831b3b79e435bd06e4f07569168d
Anders Evenrud <andersevenrud@gmail.com>
2018-10-27 03:35:43 +0200

    Updated UMD build

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,7 @@ module.exports = {
   mode,
   devtool: 'source-map',
   entry: [
-    path.resolve(__dirname, 'index.js'),
-    path.resolve(__dirname, 'index.scss')
+    path.resolve(__dirname, 'src/umd.js')
   ],
   output: {
     library: 'osjsGui',
